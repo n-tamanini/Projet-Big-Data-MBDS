48f37236a53478d4cb7935e6229697fa0791434a
Alphonse Fournier <a.fournier@net.estia.fr>
2021-03-01 16:15:24 +0100

    last version

---
+++ 02_BDAHDFS/Scripts ORACLE NoSQL/Creation_Table_Immatriculation.sql
@@ -0,0 +1,14 @@
+execute 'CREATE TABLE IF NOT EXISTS IMMATRICULATION(
+    immatriculation STRING,
+    marque STRING,
+    nom STRING,
+    puissance INTEGER,
+    longueur STRING ,
+    nbPlaces INTEGER,
+    nbPortes INTEGER,
+    couleur STRING,
+    occasion STRING,
+    prix INTEGER,
+    PRIMARY KEY(immatriculation)
+       
+)';
