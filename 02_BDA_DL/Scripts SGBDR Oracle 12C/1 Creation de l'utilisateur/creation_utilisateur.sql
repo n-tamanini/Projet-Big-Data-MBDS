9664c327cd0bddbaaac94f201091ab1ecf558bfb
n-tamanini <n.tamanini@net.estia.fr>
2021-03-03 18:44:55 +0100

    try sql external tables

--- 02_BDA_DL/Scripts SGBDR Oracle 12C/1 Creation de l'utilisateur/creation_utilisateur.sql
+++ 02_BDA_DL/Scripts SGBDR Oracle 12C/1 Creation de l'utilisateur/creation_utilisateur.sql
@@ -15,3 +15,23 @@ GRANT CREATE SESSION TO GROUPE1_PROJET_BIG_DATA WITH ADMIN OPTION;
 GRANT CREATE TABLE TO GROUPE1_PROJET_BIG_DATA;
 GRANT CREATE ANY REPOSITORY TO GROUPE1_PROJET_BIG_DATA;
 ALTER USER GROUPE1_PROJET_BIG_DATA quota unlimited ON users;
+
+
+
+
+/*
+ALTERNATIVE SUR LA MACHINE VIRTUELLE 
+*/
+
+
+-- Dans la machine virtuelle oracle@bigdatalite (local)
+-- Dans un invite de commandes
+sqlplus system@orcl/welcome1
+
+-- Création du user GROUPE1_PROJET qui va contenir toutes les tables externes dans ORACLE SQL
+CREATE USER GROUPE1_PROJET IDENTIFIED BY GROUPE1_PROJET01 
+default tablespace users
+temporary tablespace temp;
+
+grant dba to GROUPE1_PROJET;
+alter user GROUPE1_PROJET quota unlimited on users;
--- 02_BDA_DL/Scripts_ORACLE_NoSQL/03_commandes_vm_oracle_sql.sql
+++ 02_BDA_DL/Scripts_ORACLE_NoSQL/03_commandes_vm_oracle_sql.sql
@@ -1,64 +1,114 @@
--- créer les deux directories suivantes si pas cré� : 
+-------------------------------------------------------------------------------------------------------------------------------
+-- Création de la table externe ORACLE SQL pointant vers la table MARKETING de HIVE (importée dans 02_commandes_vm_hive.sql) --
+-------------------------------------------------------------------------------------------------------------------------------
+
+-- Dans la machine virtuelle oracle@bigdatalite (local)
+-- Dans un invite de commandes
+-- Connexion à l'utilisateur GROUPE1_PROJET
+sqlplus GROUPE1_PROJET@orcl/GROUPE1_PROJET01
+
+
+-- On crée les deux directories suivantes :
 -- ORACLE_BIGDATA_CONFIG et 
 -- ORA_BIGDATA_CL_bigdatalite. 
 -- La directorie ORACLE_BIGDATA_CONFIG sert à stocker les lignes
--- rappatri�es des bases distantes.
--- Op�ration à faire une seule fois.
--- v�rification
+-- rappatriées des bases distantes.
+create or replace directory ORACLE_BIGDATA_CONFIG as '/u01/bigdatasql_config';
+create or replace directory "ORA_BIGDATA_CL_bigdatalite" as '';
 
-$ sqlplus GROUPE1_PROJET_BIG_DATA@PDBEST21/GROUPE1_PROJET_BIG_DATA01
+-- Vérification
+select DIRECTORY_NAME from dba_directories;
 
+-- table externe Oracle SQL MARKETING_EXT pointant vers la table 
+-- externe HIVE correspondante (MARKETING)
+drop table MARKETING_EXT;
 
-SQL> select DIRECTORY_NAME from dba_directories;
-select * from dba_directories;
-SQL> create or replace directory ORACLE_BIGDATA_CONFIG as '/u01/bigdatasql_config';
-SQL> create or replace directory "ORA_BIGDATA_CL_bigdatalite" as '';
-
--- v�rification
-SQL> select DIRECTORY_NAME from dba_directories;
-
--- Etape 7.1.3.4 se connecter avec votre compte si pas fait.
-$ sqlplus GROUPE1_PROJET_BIG_DATA@PDBEST21/GROUPE1_PROJET_BIG_DATA01
-
--- Etape 7.1.3.5 r�er les tables externes Oracle CRITERES_yourLogin_MIAGE_ONS_EXT, 
--- APPRECIATIONS_yourLogin_MIAGE_ONS_EXT
--- CLIENTS_yourLogin_MIAGE_ONS_EXT pointant vers les tables externes HIVE �quivalentes.
-
--- table externe Oracle SQL CRITERES_yourLogin_MIAGE_ONS_EXT pointant vers la table 
--- externe HIVE
-drop table MARKETING;
-
-CREATE TABLE  MARKETING(
-CLIENTMARKETINGID number(5), 
-AGE number(2),
-SEXE varchar2(30),
-TAUX number(8),
-SITUATIONFAMILIALE varchar2(30), 
-NBENFANTSACHARGE number(2),
-DEUXIEMEVOITURE varchar2(10)
+CREATE TABLE  MARKETING_EXT(
+    CLIENTMARKETINGID number(5), 
+    AGE number(2),
+    SEXE varchar2(30),
+    TAUX number(8),
+    SITUATIONFAMILIALE varchar2(30), 
+    NBENFANTSACHARGE number(2),
+    DEUXIEMEVOITURE varchar2(10)
 )
 ORGANIZATION EXTERNAL (
-TYPE ORACLE_HIVE 
-DEFAULT DIRECTORY ORACLE_BIGDATA_CONFIG
-ACCESS PARAMETERS 
+    TYPE ORACLE_HIVE 
+    DEFAULT DIRECTORY ORACLE_BIGDATA_CONFIG
+    ACCESS PARAMETERS 
 (
-com.oracle.bigdata.tablename=default.MARKETING
+    com.oracle.bigdata.tablename=default.MARKETING
 )
 ) 
 REJECT LIMIT UNLIMITED;
 
 
 
+-- Vérification du nombre de lignes de la table MARKETING_EXT
+select count (*) from MARKETING_EXT;
+
+  COUNT(*)
+----------
+	21
+
+
+-- Vérification du contenu de la table MARKETING_EXT
+set linesize 150
+set pagesize 100
+select * from MARKETING_EXT;
+
+CLIENTMARKETINGID	 AGE SEXE	TAUX            SITUATIONFAMILIALE	 NBENFANTSACHARGE DEUXIEMEVO
+----------------- ---------- -------------------------- ---------- ---------------------------- 
+	       17	  22 M					   411 En Couple				     3 true
+	       10	  64 M					   559 C?libataire				     0 false
+	       19	  54 F					   452 En Couple				     3 true
+	       14	  19 F					   212 C?libataire				     0 false
+		    9	  43 F					   431 C?libataire				     0 false
+	       20	  35 M					   589 C?libataire				     0 false
+	       21	  59 M					   748 En Couple				     0 true
+	       15	  34 F					  1112 En Couple				     0 false
+		    2	  21 F					  1396 C?libataire				     0 false
+	       12	  79 F					   981 En Couple				     2 false
+	       18	  58 M					  1192 En Couple				     0 false
+	    	4	  48 M					   401 C?libataire				     0 false
+	    	7	  27 F					   153 En Couple				     2 false
+	    	8	  59 F					   572 En Couple				     2 false
+	       11	  22 M					   154 En Couple				     1 false
+	       13	  55 M					   588 C?libataire				     0 false
+	       16	  60 M					   524 En Couple				     0 true
+		    3	  35 M					   223 C?libataire				     0 false
+		    5	  26 F					   420 En Couple				     3 true
+		    1	     sexe				       situationFamiliale
+		    6	  80 M					   530 En Couple				     3 false
+
+21 rows selected.
+
+
+-- Vérification de la structure de la table MARKETING_EXT
+desc MARKETING_EXT;
+
+ Name					   Null?    Type
+ ----------------------------------------- -------- 
+ CLIENTMARKETINGID				    NUMBER(5)
+ AGE						        NUMBER(2)
+ SEXE						        VARCHAR2(30)
+ TAUX						        NUMBER(8)
+ SITUATIONFAMILIALE				    VARCHAR2(30)
+ NBENFANTSACHARGE				    NUMBER(2)
+ DEUXIEMEVOITURE				    VARCHAR2(10)
 
 
 
 
+/*
 
-drop table marketing;
+A TESTER
+
+drop table MARKETING_EXT;
 
  
 
-CREATE TABLE  marketing(
+CREATE TABLE  MARKETING_EXT(
 clientMarketingId NUMBER GENERATED ALWAYS AS IDENTITY constraint pk_clientMarketingId primary key,
     age     number(2) 
         constraint chk_marketing_age
@@ -88,3 +138,6 @@ com.oracle.bigdata.tablename=MARKETING
 )
 )
 REJECT LIMIT UNLIMITED;
+
+
+*/
