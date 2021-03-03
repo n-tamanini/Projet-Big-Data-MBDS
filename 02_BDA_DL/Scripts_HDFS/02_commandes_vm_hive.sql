2c0953fb89135233ef239beea3470b8b1a0436ba
n-tamanini <n.tamanini@net.estia.fr>
2021-03-03 11:54:36 +0100

    immatriculations dans hdfs et hive

---
+++ 02_BDA_DL/Scripts_HDFS/02_commandes_vm_hive.sql
@@ -0,0 +1,45 @@
+-- création de la table externe HIVE pointant vers le fichier HDFS /projet_big_data/Immatriculations.csv
+
+
+-- lancer Hive
+
+[oracle@bigdatalite ~]$ beeline
+Beeline version 1.1.0-cdh5.4.0 by Apache Hive
+
+-- Se connecter à HIVE
+
+beeline>   !connect jdbc:hive2://localhost:10000
+
+Enter username for jdbc:hive2://localhost:10000: oracle
+Enter password for jdbc:hive2://localhost:10000: ********
+(password : welcome1)
+
+-- créer la table externe HIVE pointant vers le fichier Immarticulations.csv
+
+-- table externe Hive IMMATRICULATION
+drop table IMMATRICULATION;
+
+CREATE EXTERNAL TABLE IMMATRICULATION (IMMATRICULATION STRING, MARQUE STRING,  NOM STRING, PUISSANCE  int, LONGUEUR STRING, NBPORTES int, NBPLACES int, COULEUR STRING, OCCASION STRING, PRIX INT)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+STORED AS TEXTFILE LOCATION 'hdfs:/projet_big_data';
+
+-- Vérification de la table externe IMMATRICULATION dans HIVE
+
+jdbc:hive2://localhost:10000> SELECT COUNT(*) FROM IMMATRICULATION;
+
++----------+--+
+|   _c0    |
++----------+--+
+| 2000001  |
++----------+--+
+1 row selected (92.616 seconds)
+
+
+jdbc:hive2://localhost:10000> SELECT * FROM IMMATRICULATION WHERE IMMATRICULATION.IMMATRICULATION = '4030 YB 47';
+
++----------------------------------+-------------------------+----------------------+----------------------------+---------------------------+---------------------------+---------------------------+--------------------------+---------------------------+-----------------------+--+
+| immatriculation.immatriculation  | immatriculation.marque  | immatriculation.nom  | immatriculation.puissance  | immatriculation.longueur  | immatriculation.nbportes  | immatriculation.nbplaces  | immatriculation.couleur  | immatriculation.occasion  | immatriculation.prix  |
++----------------------------------+-------------------------+----------------------+----------------------------+---------------------------+---------------------------+---------------------------+--------------------------+---------------------------+-----------------------+--+
+| 4030 YB 47                       | Volvo                   | S80 T6               | 272                        | tr�s longue               | 5                         | 5                         | bleu                     | false                     | 50500                 |
++----------------------------------+-------------------------+----------------------+----------------------------+---------------------------+---------------------------+---------------------------+--------------------------+---------------------------+-----------------------+--+
+1 row selected (2.455 seconds)
