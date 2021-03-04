1944c02ad6e4d464405d46db6262bee9e17a2d9e
n-tamanini <n.tamanini@net.estia.fr>
2021-03-04 11:28:51 +0100

    Update Nettoyage_donnees_sql.sql

--- 02_BDA_DL/Scripts_SGBDR_Oracle_12C/4 Nettoyage donnees/Nettoyage_donnees_sql.sql
+++ 02_BDA_DL/Scripts_SGBDR_Oracle_12C/4 Nettoyage donnees/Nettoyage_donnees_sql.sql
@@ -20,14 +20,13 @@ SET sexe = 'M'
 WHERE sexe = 'Homme';
 
 -- Suppression des données non conformes
-DELETE * FROM CLIENT
-WHERE sexe != 'M' OR sexe != 'F';
+DELETE FROM CLIENT
+WHERE sexe NOT IN ('M','F');
 
 -- NETTOYAGE COLONNE X2emeVoiture
 -- Suppression des données non conformes
-DELETE deuxiemeVoiture FROM CLIENT
-WHERE deuxiemeVoiture != 'true' OR deuxiemeVoiture != 'false';
-
+DELETE FROM CLIENT
+WHERE deuxiemeVoiture NOT IN ('true','false');
 
 --NETTOYAGE COLONNE SITUATIONFAMILIALE
 --Vérification des données
@@ -38,18 +37,26 @@ FROM CLIENT;
 --Les 'Seule' et 'Seul' changent pour 'Célibataire'
 UPDATE CLIENT
 SET SITUATIONFAMILIALE = 'Celibataire'
-WHERE SITUATIONFAMILIALE='Seule' OR 'Seul';
+WHERE SITUATIONFAMILIALE IN('Seule','Seul');
 
  
 --Suppression des données non conformes
-DELETE *
-FROM CLIENT
-WHERE SITUATIONFAMILIALE LIKE 'Divorc%e';
 
+UPDATE CLIENT
+SET SITUATIONFAMILIALE = 'Celibataire'
+WHERE SITUATIONFAMILIALE like 'C%libataire';
+
+UPDATE CLIENT
+SET SITUATIONFAMILIALE = 'En Couple'
+WHERE SITUATIONFAMILIALE like 'Mari%(e)';
+
+DELETE
+FROM CLIENT
+WHERE SITUATIONFAMILIALE NOT IN ('En Couple', 'Celibataire');
 
 --NETTOYAGE COLONNE NBENFANTSACHARGE
 
 --Suppression des données invalides
-DELETE *
+DELETE
 FROM CLIENT
 WHERE NBENFANTSACHARGE NOT BETWEEN 0 AND 4;
