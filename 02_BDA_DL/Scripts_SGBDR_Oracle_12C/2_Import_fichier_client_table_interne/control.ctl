5bbf95f4fac2451241b2f470fc2549d66bad01aa
n-tamanini <n.tamanini@net.estia.fr>
2021-03-04 10:48:19 +0100

    import table client

--- 02_BDA_DL/Scripts_SGBDR_Oracle_12C/2_Import_fichier_client_table_interne/control.ctl
+++ 02_BDA_DL/Scripts_SGBDR_Oracle_12C/2_Import_fichier_client_table_interne/control.ctl
@@ -1,6 +1,6 @@
 OPTIONS (SKIP=1, ERRORS=100000)
 LOAD DATA
-INFILE 'C:\data_groupe_1\Clients_0.csv'
+INFILE '/home/oracle/data_group_1/Clients_0.csv'
 INSERT INTO TABLE client
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 TRAILING NULLCOLS
---
+++ 02_BDA_DL/Scripts_SGBDR_Oracle_12C/2_Import_fichier_client_table_interne/import_table_client.sql
@@ -0,0 +1,72 @@
+sqlplus /nolog
+
+define MYDBUSER=GROUPE1_PROJET
+define MYDB=orcl
+define MYDBUSERPASS=GROUPE1_PROJET01
+
+connect &MYDBUSER@&MYDB/@MYDBUSERPASS
+
+-- Création de la table client (dans ORACLE SQL sur la machine virtuelle)
+DROP TABLE CLIENT CASCADE CONSTRAINTS;
+
+CREATE TABLE CLIENT (
+	AGE  number(2), 
+	SEXE varchar2(30),
+	TAUX number(8),
+	SITUATIONFAMILIALE varchar2(30),
+	NBENFANTSACHARGE number(2),
+	DEUXIEMEVOITURE varchar2(10),
+	IMMATRICULATION varchar2(100) 
+);
+
+-- Import des données dans la table client depuis Clients_0.csv via sqlloader
+-- Les données Clients_0.csv ont été placées dans le répertoire suivant sur la machine virtuelle (en local) : /home/oracle/data_group_1/Clients_0.csv
+
+-- Dans un invite de commandes sur la machine virtuelle : 
+MYDBUSER=GROUPE1_PROJET
+MYDB=orcl
+MYDBUSERPASS=GROUPE1_PROJET01
+MYPROJECTPATH=/home/oracle/Projet-Big-Data-MBDS
+
+cd $MYPROJECTPATH/02_BDA_DL/Scripts_SGBDR_Oracle_12C/2_Import_fichier_client_table_interne
+
+sqlldr userid=$MYDBUSER@$MYDB/$MYDBUSERPASS control=control.ctl log=track.log
+
+
+
+-- Vérification de l'import de la table interne CLIENT 
+
+sqlplus /nolog
+
+define MYDBUSER=GROUPE1_PROJET
+define MYDB=orcl
+define MYDBUSERPASS=GROUPE1_PROJET01
+
+connect &MYDBUSER@&MYDB/@MYDBUSERPASS
+
+SQL> select count (*) from client;
+
+  COUNT(*)
+----------
+     99710
+
+desc client;
+
+ Name						       Null?	Type
+ ----------------------------------------------------- --------
+ AGE								NUMBER(2)
+ SEXE								VARCHAR2(30)
+ TAUX								NUMBER(8)
+ SITUATIONFAMILIALE					VARCHAR2(30)
+ NBENFANTSACHARGE					NUMBER(2)
+ DEUXIEMEVOITURE					VARCHAR2(10)
+ IMMATRICULATION					VARCHAR2(100)
+
+
+select * from client where age = 84;
+
+-- Extrait de la réponse : 
+
+AGE       SEXE 		TAUX 		SITUATIONFAMILIALE 	    NBENFANTSACHARGE 	DEUXIEMEVO      IMMATRICULATION 
+-------- ------------------------------ ---------- ------------------------------ ---------------- ----------  
+84  	   M		563 		En Couple			     0                	false            7905 DR 45
